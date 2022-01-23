135a855d21195ae1af96535df878bc1efc1f4d6f
Peter Vila <peterdvila@gmail.com>
2022-01-22 22:50:45 -0800

    Corrected casing for Pokemon data fetched. More nested classes for PokeCard component.

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -22,15 +22,17 @@ function PokeCard(props) {
   }, [props.pokemon]);
 
   const { name, id, weight, types, sprites } = pokemon;
-
+  const properName = name && name[0].toUpperCase() + name.slice(1);
   return (
     <div className="card">
-      <p className="card__name">Name: {name}</p>
+      <p className="card__name">Name: {name && properName}</p>
       <p className="card__id">Id: {id}</p>
-      <p className="card__weight">Weight: {weight} Kg</p>
+      <p className="card__weight">Weight: {weight} kg</p>
       {types &&
         types.map((pokemon, i) => {
-          return <h3 key={i} className="card__type">{pokemon.type.name}</h3>;
+          const type = pokemon.type.name;
+          const properType = type && type[0].toUpperCase() + type.slice(1);
+          return <h3 key={i} className="card__type">{properType}</h3>;
         })}
       {sprites && <img src={sprites.front_default} className="card__img" alt={name} />}
     </div>
